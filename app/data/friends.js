f331526c22531c064edff0772a832726b82e3f38
NFRNO <NFRNO@Dantes-MacBook-Pro.local>
2019-05-09 19:15:37 -0700

    finished serverJS and added a friend to data. Created routes

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+let friends = [
+    {
+        name: "Roger",
+        photo: "",
+        scores: [
+            5,
+            4,
+            3,
+            4,
+            4,
+            5,
+            2,
+            3,
+            3,
+            4,
+        ]
+    }
+]
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,14 @@
+let friends = require("../data/friends");
+
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        //switch case for answers after more friends are added
+        //friends.push(req.body);
+        //res.json(true);
+    });
+};
