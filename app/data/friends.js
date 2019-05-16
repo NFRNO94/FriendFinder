53bfc2cd6f777bae2974aa3401afc251623a7631
NFRNO <NFRNO@Dantes-MacBook-Pro.local>
2019-05-16 16:28:56 -0700

    comparison to find compatible friend works now

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 let friendsArray = [
     {
-        name: "Roger",
-        photo: "img",
+        name: "Ashton Kutcher",
+        photo: "https://pmcvariety.files.wordpress.com/2019/01/Ashton-Kutcher.jpg?w=1000&h=563&crop=1",
         scores: [
             5,
             4,
@@ -12,9 +12,73 @@ let friendsArray = [
             2,
             3,
             3,
-            4,
+            4
         ]
-    }
-]
+    },
+    {
+        name: "Will Ferrel",
+        photo: "https://www.thewrap.com/wp-content/uploads/2018/12/WillFerrellRonBurgundy.jpg",
+        scores: [
+            5,
+            3,
+            3,
+            4,
+            5,
+            2,
+            4,
+            3,
+            5,
+            3
+        ]
+    },
+    {
+        name: "Kim Kardashian",
+        photo: "https://amp.businessinsider.com/images/5b2066e41ae66222008b4d30-1536-1152.jpg",
+        scores: [
+            5,
+            4,
+            4,
+            2,
+            3,
+            3,
+            5,
+            3,
+            4,
+            4
+        ]
+    },
+    {
+        name: "Ariana Grande",
+        photo: "https://consequenceofsound.net/wp-content/uploads/2018/11/ariana-grande.jpg",
+        scores: [
+            4,
+            2,
+            3,
+            4,
+            1,
+            5,
+            4,
+            1,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Jason Momoa",
+        photo: "https://cdnph.upi.com/svc/sv/upi/6301557325338/2019/1/50e728513845f0c3318396d3656e0c13/Jason-Momoa-says-hes-really-passionate-about-Aquaman-2.jpg",
+        scores: [
+            3,
+            1,
+            5,
+            2,
+            5,
+            2,
+            3,
+            2,
+            4,
+            2
+        ]
+    },
+];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,11 +7,40 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        //switch case for answers after more friends are added
-        if (friends) {
-            friends.push(req.body);
-            res.json(true);
+    
+        let bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: Infinity
         }
 
-    });
+        let userData = req.body;
+        let userScores = userData.scores;
+
+        let totalDifference;
+
+        for (let i = 0; i < friends.length; i++) {
+            let currentFriend = friends[i];
+            totalDifference = 0;
+
+            console.log(currentFriend.name);
+
+            for (let j = 0; j < currentFriend.scores.length; j++) {
+                let currentFriendScore = currentFriend.scores[j];
+                let currentUserScore = userScores[j];
+
+                totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+            }
+
+            if (totalDifference <= bestMatch.friendDifference) {
+                bestMatch.name = currentFriend.name;
+                bestMatch.photo = currentFriend.photo;
+                bestMatch.friendDifference = totalDifference;
+              }
+            }
+        
+            friends.push(userData);
+
+            res.json(bestMatch);
+        });
 };
